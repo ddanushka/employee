87237045834e5d688e52053c22112d2172e56e22
Dilshan Danushka <dilshan@convertium.com>
2023-02-22 03:52:12 +0530

    code clean up

--- src/pages/Chart.tsx
+++ src/pages/Chart.tsx
@@ -31,10 +31,36 @@ const Chart: React.FC = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const getEmployees = await API.getEmployees();
-      createChartData(getEmployees);
-    };
+      const employees = await API.getEmployees();
+      const years: { [key: string]: number } = {};
+      pastYears(10).forEach((y) => {
+        years[y] = 0;
+      })
+      employees.forEach((employee: Employee) => {
+        const year = moment(employee.joinedDate).year();
+        if (!years[year]) {
+          years[year] = 0;
+        }
 
+        years[year]++;
+      });
+
+      const labels = Object.keys(years);
+      const data = Object.values(years);
+
+      setChartData({
+        labels,
+        datasets: [
+          {
+            label: "Number of Employees",
+            data,
+            backgroundColor: "#bfebc5",
+            borderColor: "#00ad18",
+            borderWidth: 1
+          }
+        ]
+      });
+    };
     fetchData();
   }, []);
 
@@ -53,39 +79,8 @@ const Chart: React.FC = () => {
 
   const pastYears = (back: number) => {
     const year = new Date().getFullYear();
-    return Array.from({length: back}, (v, i) => year - back + i + 1);
+    return Array.from({ length: back }, (v, i) => year - back + i + 1);
   }
-
-  const createChartData = (employees: Employee[]) => {
-    const years: { [key: string]: number } = {};
-    pastYears(10).forEach((y)=>{
-      years[y] = 0;
-    })
-    employees.forEach((employee: Employee) => {
-      const year = moment(employee.joinedDate).year();
-      if (!years[year]) {
-        years[year] = 0;
-      }
-
-      years[year]++;
-    });
-
-    const labels = Object.keys(years);
-    const data = Object.values(years);
-
-    setChartData({
-      labels,
-      datasets: [
-        {
-          label: "Number of Employees",
-          data,
-          backgroundColor: "#bfebc5",
-          borderColor: "#00ad18",
-          borderWidth: 1
-        }
-      ]
-    });
-  };
 
   return (
     <div>
